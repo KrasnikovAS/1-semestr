561c09da3a19cf56d4e4ed690fd01a5f8aecb098
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-10-16 01:43:03 +0300

    Add files via upload

---
+++ ПЗ9/23_Krasnikov/23_Krasnikov/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите сумму вклада:");
+        decimal deposit = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Введите количество месяцев:");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        decimal result = deposit;
+        decimal p = 0.07m;
+
+        for (int i = 0; i < months; i++)
+        {
+            decimal interest = result * p;
+            result += interest;
+        }
+
+        Console.WriteLine($"Конечная сумма вклада: {result:C}");
+    }
+}
---
+++ ПЗ9/23_Krasnikov/23_Krasnikov2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите сумму вклада:");
+        decimal deposit = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Введите количество месяцев:");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        decimal result = deposit;
+        decimal p = 0.07m;
+
+        int i = 0;
+        while (i < months)
+        {
+            decimal interest = result * p;
+            result += interest;
+            i++;
+        }
+
+
+        Console.WriteLine($"Конечная сумма вклада: {result:C}");
+    }
+}
