53d3fb1d0fa02b23d5ad8ab86773d98782074c47
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-10-16 01:43:46 +0300

    Add files via upload

---
+++ ПЗ9/23_Krasnikov/23_Krasnikov4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+
+        while (true)
+        {
+            Console.WriteLine("Введите первое число:");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            if ((n1 >=0 && n1 <= 10)&&(n2 >=0 && n2 <= 10))
+            {
+                Console.WriteLine(n1 * n2);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Введенные числа недопустимы");
+                continue;
+            }
+        }
+    }
+}
