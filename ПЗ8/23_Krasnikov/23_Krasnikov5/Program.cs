eeaf7328806d37f33b0c9a1b434336be7e4a16cd
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-10-16 00:23:51 +0300

    Add files via upload

---
+++ ПЗ8/23_Krasnikov/23_Krasnikov5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            double deposit = Convert.ToDouble(Console.ReadLine());
+            double interest1;
+            double bonus = 15;
+
+            if (deposit < 100)
+            {
+                interest1 = 0.05;
+            }
+            else if (deposit >= 100 || deposit <= 200)
+            {
+                interest1 = 0.07;
+            }
+            else
+            {
+                interest1 = 0.1;
+            }
+            double intetest2 = deposit * interest1;
+            double result = deposit + intetest2 + bonus;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ПЗ8/23_Krasnikov/23_Krasnikov6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Сложение");
+                    break;
+                case 2:
+                    Console.WriteLine("Вычитание");
+                    break;
+                case 3:
+                    Console.WriteLine("Умножение");
+                    break;
+                default:
+                    Console.WriteLine("Операция неопределена");
+                    break;
+            }
+        }
+    }
+}
