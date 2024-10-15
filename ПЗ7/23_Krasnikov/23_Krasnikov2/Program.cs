5cbd4cc23ae89a2bb7c88093f7108da57600213f
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-10-15 23:15:47 +0300

    Add files via upload

---
+++ ПЗ7/23_Krasnikov/23_Krasnikov2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindInString();
+            DeleteString();
+            ReplaceString();
+            UpperString();
+        }
+        static void FindInString()
+        {
+            string s1 = "Я скоро поеду на научную конференцию в Курск";
+
+            string subString = "Курск";
+            int indexOfSubstring = s1.IndexOf(subString);
+            Console.WriteLine("Индекс подстроки в строке {0} - {1}", subString, indexOfSubstring);
+        }
+
+        static void DeleteString()
+        {
+            string s1 = "Я скоро поеду на научную конференцию в Курск";
+            int ind = s1.Length - 8;
+            s1 = s1.Remove(ind);
+            Console.WriteLine(s1);
+        }
+        static void ReplaceString()
+        {
+            string s1 = "Я скоро поеду на научную конференцию в Курск";
+            s1 = s1.Replace("научную конференцию", "соревнование");
+            Console.WriteLine(s1);
+        }
+        static void UpperString()
+        {
+            string s1 = "Я скоро поеду на научную конференцию в Курск";
+            Console.WriteLine(s1.ToUpper());
+        }
+    }
+}
