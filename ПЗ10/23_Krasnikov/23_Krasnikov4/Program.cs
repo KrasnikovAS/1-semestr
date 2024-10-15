46095066a4f374b3d68f1d8bd4aa207c41d7582e
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-10-16 02:51:38 +0300

    Add files via upload

---
+++ ПЗ10/23_Krasnikov/23_Krasnikov4/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+using static System.Math;
+
+class Program
+{
+    static void Main()
+    {
+        Write("Введите координату x (1-8): ");
+        int x = int.Parse(ReadLine());
+
+        Write("Введите координату y (1-8): ");
+        int y = int.Parse(ReadLine());
+
+        bool isWhite = (x + y) % 2 == 1;
+
+        WriteLine($"Клетка с координатами ({x}, {y}) белая: {isWhite}");
+    }
+}
