8c699f42045d5c29ff5a3a746442d2d8bd09b811
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-12-16 02:57:22 +0300

    Add files via upload

---
+++ ПЗ6/23K_Krasnikov 2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Test
+{
+    class BasicIO
+    {
+        static void Main()
+        {
+            string str;
+
+            str = "Фамилия: {{ {0} }}, дата рождения: {{ {1} }}, место рождения: {{ {2} }}, возраст: {{ {3} }}";
+            str = String.Format(str, "Васильев", "10.10.2020", "г.Курск", 24);
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ ПЗ6/23K_Krasnikov 4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Test
+{
+    class BasicIO
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите номер телефона");
+
+            long number = Convert.ToInt64(Console.ReadLine());
+            string result = String.Format("{0:+# (####) ##-##-##}", number);
+            Console.WriteLine($"Номер телефона: {result}");
+        }
+    }
+}
