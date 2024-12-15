a6c1afd259e92812d98072d2041eea93238576d2
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-12-16 02:17:28 +0300

    ПЗ5

---
+++ ПЗ5/IS23_F1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        int maxNumber = Math.Max(number1, number2);
+
+        Console.WriteLine($"Наибольшее число: {maxNumber}");
+    }
+}
---
+++ ПЗ5/IS23_F2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+        Console.WriteLine($"c = {c:F2}");
+    }
+}
