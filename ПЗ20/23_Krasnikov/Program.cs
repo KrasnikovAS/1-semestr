4429776b198fe86412012e33f64550d03d2eab59
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-12-23 04:57:08 +0300

    Add files via upload

---
+++ ПЗ20/23_Krasnikov/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите последовательность чисел, заканчивающуюся 0:");
+        int result = Sum();
+        Console.WriteLine($"Сумма всех чисел: {result}");
+    }
+
+    static int Sum()
+    {
+        int temp = Convert.ToInt32(Console.ReadLine());
+
+        if (temp == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return temp + Sum();
+        }
+    }
+}
