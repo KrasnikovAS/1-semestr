9740074298edbfd243f09d7817f6a1198a61a4b5
KrasnikovIS23k <robloxkubik1337@gmail.com>
2024-12-23 04:24:58 +0300

    Add files via upload

---
+++ ПЗ17/23_Krasnikov/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "..\\..\\..\\numbers.txt";
+
+        try
+        {
+            string numbers = string.Join(",", Enumerable.Range(1, 500));
+
+            // Запись в файл
+            File.WriteAllText(filePath, numbers);
+
+            Console.WriteLine($"Файл '{filePath}' успешно создан.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ ПЗ17/23_Krasnikov3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Введите полный путь к текстовому файлу: ");
+            string filePath = Console.ReadLine();
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не найден. Убедитесь, что путь указан правильно.");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            int maxLength = 0;
+            foreach (string line in lines)
+            {
+                if (line.Length > maxLength)
+                {
+                    maxLength = line.Length;
+                }
+            }
+
+            Console.WriteLine($"Длина самой длинной строки: {maxLength}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
